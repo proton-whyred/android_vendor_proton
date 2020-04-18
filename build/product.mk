c6c7d2e8112a805041e0743a52b153f0a4b1b7b4
Chris Crump <chris@cypheros.co>
2020-04-17 23:31:54 -0400

    proton: Add FaceSense stuff
    Change-Id: I059a6aabfdfea8a2fceccbfc809a8fa8d8370098

--- build/product.mk
+++ build/product.mk
@@ -21,6 +21,7 @@ $(call inherit-product, vendor/proton/telephony/telephony.mk)
 $(call inherit-product, vendor/proton/audio/audio.mk)
 $(call inherit-product-if-exists, vendor/proton/signing/dev.mk)
 $(call inherit-product, vendor/proton/build/qcom_boards.mk)
+$(call inherit-product, vendor/proton/aospa/facesense/facesense.mk)
 
 # Flatten APEXs for performance
 # TODO: fix Pixel 6 camera HAL flattening and re-enable this
